c1a38cce393c22d8a698934f9d8b99fad86f4c89
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-12-07 16:37:26 +0600

    added footer

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -5,6 +5,7 @@ import HeroSection from './components/HeroSection/HeroSection'
 import Trending from './components/Sections/Trending'
 import Category from './components/Sections/Categories/Category'
 import content from './data/content.json'
+import Footer from './components/Footer/Footer'
 
 function Shop() {
 
@@ -14,6 +15,7 @@ function Shop() {
       <HeroSection />
       <Trending />
       {content?.categories && content?.categories?.map((item, index) => <Category key={item?.title + index} {...item}/>)}
+      <Footer content={content?.footer}/>
     </>
   )
 }
